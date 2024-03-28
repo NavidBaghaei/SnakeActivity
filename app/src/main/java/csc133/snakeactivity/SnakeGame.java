2a03c42ee3b8bc36c7f3fd5ea653b01c1a93b073
Navid Baghaei <navidbaghaei@csus.edu>
2024-03-27 21:07:58 -0700

    Added paused text

--- app/src/main/java/csc133/snakeactivity/SnakeGame.java
+++ app/src/main/java/csc133/snakeactivity/SnakeGame.java
@@ -272,8 +272,14 @@ class SnakeGame extends SurfaceView implements Runnable{
             int y = (int) (textPaint.getTextSize() + 20); // Add some margin to the y-coordinate as well
             mCanvas.drawText(names, x, y, textPaint);
 
-            // Draw "Tap to Play" when the game is paused or not yet started
-            if (mPaused || !isGameStarted) {
+            // Draw "Paused" when the game is paused
+            if (mPaused) {
+                mPaint.setTextSize(250);
+                mCanvas.drawText("Paused", 200, 700, mPaint);
+            }
+
+            // Draw "Tap to Play" when the game has not started
+            if (!isGameStarted) {
                 mPaint.setTextSize(250);
                 mCanvas.drawText(getResources().getString(R.string.tap_to_play), 200, 700, mPaint);
             }
@@ -283,6 +289,7 @@ class SnakeGame extends SurfaceView implements Runnable{
             mSurfaceHolder.unlockCanvasAndPost(mCanvas);
         }
     }
+
 
 
     private void drawPauseButton(Canvas canvas) {
